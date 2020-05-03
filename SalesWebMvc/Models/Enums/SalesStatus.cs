f37f3ad99d884989fd08d86eb48542991fde8ebb
Fabio Giro <Fabio.Giro@sistema.corp>
2020-05-03 14:12:30 -0300

    Class 239-240 - Sellers Controler

--- SalesWebMvc/Models/Enums/SalesStatus.cs
+++ SalesWebMvc/Models/Enums/SalesStatus.cs
@@ -1,9 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace SalesWebMvc.Models.Enuns {
-    public class SalesStatus {
+﻿namespace SalesWebMvc.Models.Enums {
+    public enum SaleStatus {
+        Pending = 0,
+        Billed = 1,
+        Canceled = 2
     }
 }
