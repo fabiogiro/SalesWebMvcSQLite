824b5e68e61ba3d7793aea8be74a3928d95adf59
Fabio Giro <rodo@sistema.corp>
2020-05-07 19:26:58 -0300

    Class 249 - Format date and number

--- SalesWebMvc/Models/Seller.cs
+++ SalesWebMvc/Models/Seller.cs
@@ -1,13 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 
 namespace SalesWebMvc.Models {
     public class Seller {
         public int ID { get; set; }
         public string Name { get; set; }
+        [DataType(DataType.EmailAddress)]
         public String Email { get; set; }
+        [Display(Name = "Birth Date")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString ="{0:dd/MM/yyyy}")]
         public DateTime BirthDate { get; set; }
+        [Display(Name = "Base Salary")]
+        [DisplayFormat(DataFormatString ="{0:F2}")]
         public double BaseSalary { get; set; }
         public Department Department { get; set; }
         public int DepartmentID { get; set; }
